59682be7c4d6c5d90daf271b04620a229148fd17
Kolbanidze <137103175+kolbanidze@users.noreply.github.com>
2026-03-15 16:22:43 +0300

    clang migration fix

--- update_and_build.sh
+++ update_and_build.sh
@@ -6,50 +6,56 @@ MY_PKGBASE="linux-secux"
 CONFIG_FRAGMENT="config.secux"
 
 if [ ! -d "$REPO_DIR" ]; then
-    echo "> Клонируем linux-hardened..."
-    pkgctl repo clone --protocol=https linux-hardened
+    echo "> Клонируем $REPO_DIR..."
+    pkgctl repo clone --protocol=https $REPO_DIR
 else
-    echo "> Обновляем linux-hardened..."
-    cd $REPO_DIR
+    echo "> Обновляем $REPO_DIR..."
+    cd "$REPO_DIR"
     git reset --hard HEAD
     git clean -fdx
     git pull
     cd ..
 fi
 
-echo "> Применяем модификации..."
+echo "> Применяем модификации PKGBUILD..."
+cd "$REPO_DIR"
 
-sed -i "s/^pkgbase=.*/pkgbase=${MY_PKGBASE}/" $REPO_DIR/PKGBUILD
-sed -i 's/${pkgbase}-${_srctag}.patch/linux-hardened-${_srctag}.patch/' $REPO_DIR/PKGBUILD
-sed -i '/make htmldocs/d' $REPO_DIR/PKGBUILD
-sed -i '/"\$pkgbase-docs"/d' $REPO_DIR/PKGBUILD
-sed -i '/graphviz/d; /imagemagick/d; /python-sphinx/d; /texlive-latexextra/d' $REPO_DIR/PKGBUILD
-sed -i '/local pid_docs=$!/d' $REPO_DIR/PKGBUILD
-sed -i '/wait "${pid_docs}"/d' $REPO_DIR/PKGBUILD
+sed -i "s/^pkgbase=.*/pkgbase=${MY_PKGBASE}/" PKGBUILD
+sed -i 's/${pkgbase}-${_srctag}.patch/linux-hardened-${_srctag}.patch/' PKGBUILD
 
-# При сборке make olddefconfig отдаст приоритет последним строкам.
-cat $REPO_DIR/config.x86_64 $CONFIG_FRAGMENT > $REPO_DIR/config.x86_64.tmp
-mv $REPO_DIR/config.x86_64.tmp $REPO_DIR/config.x86_64
+sed -i '/make htmldocs/d' PKGBUILD
+sed -i '/"\$pkgbase-docs"/d' PKGBUILD
+sed -i '/graphviz/d; /imagemagick/d; /python-sphinx/d; /texlive-latexextra/d' PKGBUILD
+sed -i '/local pid_docs=$!/d' PKGBUILD
+sed -i '/wait "${pid_docs}"/d' PKGBUILD
 
-echo "> Переключаем сборку на LLVM, LTO и x86-64-v3..."
+# Инъекция LLVM и KCFLAGS во все вызовы make внутри PKGBUILD
+# Ищет точное слово make (в начале строки или после пробела) и добавляет аргументы
+sed -i -E 's/(^|[[:space:]])make([[:space:]]|$)/\1make LLVM=1 LLVM_IAS=1 KCFLAGS="-march=x86-64-v3"\2/g' PKGBUILD
 
-# LLVM=1 автоматически подтянет clang, lld, llvm-ar.
-# KCFLAGS="-march=x86-64-v3" заставит ядро использовать AVX/AVX2 инструкции.
-sed -i 's/make all/make LLVM=1 KCFLAGS="-march=x86-64-v3" all/g' $REPO_DIR/PKGBUILD
-sed -i 's/make modules_install/make LLVM=1 KCFLAGS="-march=x86-64-v3" modules_install/g' $REPO_DIR/PKGBUILD
-sed -i 's/make -s olddefconfig/make LLVM=1 -s olddefconfig/g' $REPO_DIR/PKGBUILD
+sed -i '/make.*olddefconfig/i \  # Инъекция флагов для DKMS\n  sed -i "1a LLVM=1\\nLLVM_IAS=1\\nexport LLVM LLVM_IAS" Makefile' PKGBUILD
 
-# Arch Linux по умолчанию имеет отключенный LTO в базовом конфиге (CONFIG_LTO_NONE=y).
-# Нам нужно гарантированно вырезать этот параметр перед слиянием с нашим CONFIG_FRAGMENT (config.secux)
-sed -i '/CONFIG_LTO_NONE=y/d' $REPO_DIR/config.x86_64
+echo "> Разрешение конфликтов Kconfig и слияние конфигов..."
 
-# Финальная подготовка
-cd $REPO_DIR
+# Точечное удаление дефолтных опций из блоков Choice, чтобы Kconfig не ругался
+sed -i -E '/CONFIG_(LTO_NONE|DEBUG_INFO_DWARF_TOOLCHAIN_DEFAULT|DEBUG_INFO_DWARF4|DEBUG_INFO_DWARF5|DEBUG_INFO_BTF)=/d' config.x86_64
+
+# Парсим наш фрагмент и удаляем любые старые упоминания этих ключей из базы
+grep -E '^(# )?CONFIG_' "../$CONFIG_FRAGMENT" | sed -E 's/^# (CONFIG_[^ ]+) is not set/\1/; s/^(CONFIG_[^=]+)=.*/\1/' | while read -r conf; do
+    sed -i "/^${conf}=/d" config.x86_64
+    sed -i "/^# ${conf} is not set/d" config.x86_64
+done
+
+# Присоединение нашего кастомного фрагмента
+cat "../$CONFIG_FRAGMENT" >> config.x86_64
 
 echo "> Обновляем контрольные суммы..."
 updpkgsums
 
-echo "> Запускаем сборку..."
-makepkg -s
+echo "> Настройка окружения и запуск сборки..."
+export MAKEFLAGS="-j$(nproc)"
+export COMPRESSZST=(zstd -c -T0 --ultra -20 -)
 
-echo "> Готово. Пакеты лежат в папке $REPO_DIR"
+makepkg -s -e --noconfirm
+
+echo "> Готово! Пакеты ядра $MY_PKGBASE успешно собраны через LLVM."
